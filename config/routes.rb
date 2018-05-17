392e6514f7237fbb825ef5b2e70ddeb363f27ffe
Robert Mulder <rmulder@xmission.com>
2018-05-16 21:14:32 -0700

    Save updates to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/contact'
+  get 'welcome/features'
+  get 'welcome/faqs'
+  get 'welcome/pricing'
   resources :invoices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
 end
