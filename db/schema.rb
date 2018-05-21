b789f443267ebd111209909d1747ee5610167e23
Robert Mulder <rmulder@xmission.com>
2018-05-21 09:32:27 -0700

    Save updates for status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_033201) do
+ActiveRecord::Schema.define(version: 2018_05_21_161738) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_05_21_033201) do
     t.string "salesperson"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status_type"
   end
 
   create_table "users", force: :cascade do |t|
