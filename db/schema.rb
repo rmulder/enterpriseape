7cd9042ef487d92de5451aaad0c04a71eb62bc53
Robert Mulder <rmulder@xmission.com>
2018-05-20 20:59:16 -0700

    Save changes for adding companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_20_013830) do
+ActiveRecord::Schema.define(version: 2018_05_21_033201) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "manager"
+    t.string "status"
+    t.integer "terms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
